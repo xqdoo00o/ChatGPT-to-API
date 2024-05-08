2521b46471a0cfa86bf4d9a648358efc79a22b61
xqdoo00o <xqdoo00o@gmail.com>
2024-05-09 01:14:56 +0800

    fix panic if cant access chatgpt domain

--- internal/chatgpt/request.go
+++ internal/chatgpt/request.go
@@ -72,6 +72,7 @@ func init() {
 	screen := screens[rand.Intn(3)]
 	cachedHardware = core + screen
 }
+
 func newRequest(method string, url string, body io.Reader, secret *tokens.Secret, deviceId string) (*http.Request, error) {
 	request, err := http.NewRequest(method, url, body)
 	if err != nil {
@@ -281,6 +282,8 @@ func GetDpl(proxy string) {
 	if proxy != "" {
 		client.SetProxy(proxy)
 	}
+	cachedScripts = append(cachedScripts, "https://cdn.oaistatic.com/_next/static/chunks/9598-0150caea9526d55d.js?dpl=abad631f183104e6c8a323392d7bc30b933c5c7c")
+	cachedDpl = "dpl=abad631f183104e6c8a323392d7bc30b933c5c7c"
 	request, err := http.NewRequest(http.MethodGet, "https://chatgpt.com/?oai-dm=1", nil)
 	request.Header.Set("User-Agent", userAgent)
 	request.Header.Set("Accept", "*/*")
@@ -293,11 +296,11 @@ func GetDpl(proxy string) {
 	}
 	defer response.Body.Close()
 	doc, _ := goquery.NewDocumentFromReader(response.Body)
-	cachedScripts = nil
+	scripts := []string{}
 	doc.Find("script[src]").Each(func(i int, s *goquery.Selection) {
 		src, exists := s.Attr("src")
 		if exists {
-			cachedScripts = append(cachedScripts, src)
+			scripts = append(scripts, src)
 			if cachedDpl == "" {
 				idx := strings.Index(src, "dpl")
 				if idx >= 0 {
@@ -306,16 +309,14 @@ func GetDpl(proxy string) {
 			}
 		}
 	})
-	if len(cachedScripts) == 0 {
-		cachedScripts = append(cachedScripts, "https://cdn.oaistatic.com/_next/static/chunks/polyfills-78c92fac7aa8fdd8.js?dpl=baf36960d05dde6d8b941194fa4093fb5cb78c6a")
-		cachedDpl = "dpl=baf36960d05dde6d8b941194fa4093fb5cb78c6a"
+	if len(scripts) != 0 {
+		cachedScripts = scripts
 	}
 }
 func getConfig() []interface{} {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 	script := cachedScripts[rand.Intn(len(cachedScripts))]
 	return []interface{}{cachedHardware, getParseTime(), int64(4294705152), 0, userAgent, script, cachedDpl, "zh-CN", "zh-CN,en,en-GB,en-US", 0}
-
 }
 func CalcProofToken(require *ChatRequire, proxy string) string {
 	proof := generateAnswer(require.Proof.Seed, require.Proof.Difficulty, proxy)
